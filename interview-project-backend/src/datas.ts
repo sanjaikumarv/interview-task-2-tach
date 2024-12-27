9ef17e1b2d5f543914b2c61894dbbce12f2168b7
Sanjaikumar v <80317211+sanjaikumarv@users.noreply.github.com>
2024-12-27 17:29:18 +0530

    feat : data changes

--- interview-project-backend/src/datas.ts
+++ interview-project-backend/src/datas.ts
@@ -13,15 +13,15 @@ export const sideBarItems = [
 export const mainContent = [
     {
         sideBarName: "A",
-        content: ['Element-A1', 'Element-A2', 'Element-A3', "Element-A4", "Element-A5", "Element-A6", "Element-A7", "Element-A8", "Element-A9", "Element-A10", "Element-A11", "Element-A12", "Element-A13", "Element-A14", "Element-A15", "Element-A16", "Element-A17"]
+        content: ['Element-A1', 'Element-A2', 'Element-A3']
     },
     {
         sideBarName: "B",
-        content: ['Element-B1', 'Element-B2', 'Element-B3', "Element-B4", "Element-B5", "Element-B6", "Element-B7", "Element-B8", "Element-B9", "Element-B10", "Element-B11", "Element-B12", "Element-B13", "Element-B14", "Element-B15", "Element-B16", "Element-B17"]
+        content: ['Element-B1', 'Element-B2', 'Element-B3']
     },
     {
         sideBarName: "C",
-        content: ['Element-C1', 'Element-C2', 'Element-C3', "Element-C4", "Element-C5", "Element-C6", "Element-C7", "Element-C8", "Element-C9", "Element-C10", "Element-C11", "Element-C12", "Element-C13", "Element-C14", "Element-C15", "Element-C16", "Element-C17"]
+        content: ['Element-C1', 'Element-C2', 'Element-C3']
     },
     {
         sideBarName: "X",
